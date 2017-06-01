7a276bc781fca1c1f324c63bca3c89d7a1c6762f
Mikhail Mulev <mike@ilp-agency.ru>
2017-06-01 22:02:33 +0300

    coverall

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
-require 'simplecov'
+require 'coveralls'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'alexa_ruby'
 
+Coveralls.wear!
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new(color: true)
-SimpleCov.start
